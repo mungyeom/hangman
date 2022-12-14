8f422a641989cca18ab1dfe7f1013e983c33400d
mungyeom <gyeomun.kim@gmail.com>
2022-12-14 20:57:03 +0000

    first commit

--- milestone_2.py
+++ milestone_2.py
@@ -5,8 +5,8 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 
-guess = input('enter a single letter')
-if len(guess) == 1 and type(guess) == str:
+guess = input('enter a single letter ')
+if len(guess) == 1 and guess.isalpha() == True:
     print( "Good guess!")
 else:
     print("Oops! That is not a valid input.")
