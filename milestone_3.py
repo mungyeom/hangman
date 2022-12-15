a17b14f35c87586b0855e91c9ecb89227c7ce643
mungyeom <gyeomun.kim@gmail.com>
2022-12-15 13:52:06 +0000

    second commit

--- milestone_3.py
+++ milestone_3.py
@@ -13,24 +13,25 @@ if guess in word:
 else:
     print("Sorry, {0} is not in the word. Try again." .format(guess))
 
-def check_guess():
-    while True:
-        guess = input('enter a single letter ')
-        if len(guess) == 1 and guess.isalpha() == True:
-            return guess.lower()
-            break
-        else:
-            print('Invalid letter. Please, enter a single alphabetical character.')
-
-check_guess()
-
 word = 'apple'
 
-def ask_for_input():
-    guess = check_guess()
+def check_guess(guess):
+    guess = guess.lower()
     if guess in word:
         print("Good guess! {0} is in the word." .format(guess))
     else:
         print("Sorry, {0} is not in the word. Try again." .format(guess))
+
+check_guess(guess)
+
+
+def ask_for_input():
+    while True:
+        guess = input('enter a single letter ')
+        if len(guess) == 1 and guess.isalpha() == True:
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+    check_guess(guess)  
 
 ask_for_input()
