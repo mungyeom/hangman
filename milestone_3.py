3d9651682543908ae0892d5620a9cfb3f65e1337
mungyeom <gyeomun.kim@gmail.com>
2022-12-15 12:13:39 +0000

    second commit

--- milestone_3.py
+++ milestone_3.py
@@ -1,7 +1,19 @@
-
 while True:
     guess = input('enter a single letter ')
     if len(guess) == 1 and guess.isalpha() == True:
         break
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print('Invalid letter. Please, enter a single alphabetical character.')
+
+
+word = 'peach'
+while True: 
+    guess = input('enter a single letter ')
+    if  guess in word:
+        print("Good guess! {0} is in the word." .format(guess))
+        break
+    else:
+        print("Sorry, {0} is not in the word. Try again." .format(guess))
+
+
+
