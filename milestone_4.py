3e7f8a0b9bfb2a6f94009abaa989d13a2a276e9e
mungyeom <gyeomun.kim@gmail.com>
2022-12-19 19:52:12 +0000

    third commit

--- milestone_4.py
+++ milestone_4.py
@@ -17,17 +17,15 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print("Good guess! {0} is in the word." .format(guess))
-            self.num_lives -= 1
             for i in range(0,len(self.word)):
                 letter = self.word[i]
                 if guess == letter:
                     self.word_guessed[i] = guess
                     print(self.word_guessed)
-                   
         else:
             self.num_lives -= 1
-            print(f"Sorry, {guess} is not in the word.")
-            print(f"You have {self.num_lives} lives left.")
+            # print(f"Sorry, {guess} is not in the word.")
+            # print(f"You have {self.num_lives} lives left.")
         
            
 
